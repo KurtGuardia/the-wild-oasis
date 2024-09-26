1d8e32c4a66e7e180177660a65d60948764e1ed6
Kurt Guardia <kurtguardia@gmail.com>
2024-09-26 11:34:16 +0200

    Dashboard top stats and bottom bookings line chart done

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,43 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { useRecentBookings } from './useRecentBookings'
+import { useRecentStays } from './useRecentStays'
+import { useCabins } from '../cabins/useCabins'
+import Stats from './Stats'
+import SalesChart from './SalesChart'
+import Spinner from '../../ui/Spinner'
 
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
-`;
+`
+
+export default function DashboardLayout() {
+  const { bookings, isLoading: isLoading1 } =
+    useRecentBookings()
+  const {
+    stays,
+    confirmedStays,
+    numDays,
+    isLoading: isLoading2,
+  } = useRecentStays()
+  const { cabins, isLoading: isLoading3 } = useCabins()
+
+  if (isLoading1 || isLoading2 || isLoading3)
+    return <Spinner />
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Today&apos;s activity</div>
+      <div>Chart stay durations</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
+    </StyledDashboardLayout>
+  )
+}
